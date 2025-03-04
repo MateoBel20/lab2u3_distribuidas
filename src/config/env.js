5f961048964afaf9ffbae8f84804acb6b05bad36
Mateo Beltran <msbeltran@espe.edu.ec>
2025-03-03 22:29:56 -0500

    Arreglo de archivo env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 module.exports = {
-  environment: process.env.NODE_ENV || 'development',
+  environment: process.env.NODE_ENV || 'production',
   databaseUrl: process.env.DATABASE_URL,
   port: process.env.PORT || 8080,
 };
