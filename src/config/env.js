29c2ff148093d122c8b0fe023ef887ec2107a8dd
Mateo Beltran <msbeltran@espe.edu.ec>
2025-03-03 22:31:40 -0500

    Arreglo de puertos

--- src/config/env.js
+++ src/config/env.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 module.exports = {
   environment: process.env.NODE_ENV || 'production',
   databaseUrl: process.env.DATABASE_URL,
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 3000,
 };
