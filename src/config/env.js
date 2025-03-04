5beb14099ae11d95dcbc8e61110900546f201957
Mateo Beltran <msbeltran@espe.edu.ec>
2025-03-03 22:25:26 -0500

    Configuración de puerto

--- src/config/env.js
+++ src/config/env.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 module.exports = {
   environment: process.env.NODE_ENV || 'development',
   databaseUrl: process.env.DATABASE_URL,
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 8080,
 };
